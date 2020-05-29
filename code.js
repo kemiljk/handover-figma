9994e90eb9d0f0e5bbc3abdad27644dd365a6b35
Karl Koch <hi@kejk.co>
2020-05-29 22:55:27 +0100

    fix: added in notification message when a text layer isn't selected

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 figma.showUI(__html__, { width: 300, height: 300 });
+figma.notify("Please select a text layer to use Px ›› Ems");
 const selection = figma.currentPage.selection["0"].fontSize;
 /* console.log(figma.currentPage.selection["0"].fontSize); */
 figma.ui.postMessage({ "fontSize": selection });
--- code.ts
+++ code.ts
@@ -1,8 +1,9 @@
 figma.showUI(__html__, { width: 300, height: 300 });
+figma.notify("Please select a text layer to use Px ›› Ems");
 
 const selection = figma.currentPage.selection["0"].fontSize;
 /* console.log(figma.currentPage.selection["0"].fontSize); */
-figma.ui.postMessage({ "fontSize": selection });
+figma.ui.postMessage({ "fontSize": selection })
 
 figma.ui.onmessage = () => {
   figma.closePlugin();
