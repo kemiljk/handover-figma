23dd0d1fb43e292c2674cb3bcfd9b747143d0ed4
Karl Koch <hi@kejk.co>
2020-05-30 09:57:09 +0100

    feat: Added abilty to update the value based on changing the text node selection rather than requiring manual entry or rerunning the plugin. Also updated input to highlight orange

--- code.ts
+++ code.ts
@@ -1,11 +1,18 @@
 figma.showUI(__html__, { width: 300, height: 300 });
 
 if (figma.currentPage.selection.length !== 1) {
-  figma.notify("Select a single text block to continue.")
+  figma.notify("Select a single text block to get the selection value.")
 }
-  
-var selection = figma.currentPage.selection["0"].fontSize;
-figma.ui.postMessage({ "fontSize": selection });
+
+if (figma.currentPage.selection.length === 1) {
+  var selection = figma.currentPage.selection["0"].fontSize;
+  figma.ui.postMessage({ "fontSize": selection });
+}
+
+figma.on("selectionchange", () => { 
+  var selection = figma.currentPage.selection["0"].fontSize;
+  figma.ui.postMessage({ "fontSize": selection });
+})
 
 figma.ui.onmessage = () => {
   figma.closePlugin();
